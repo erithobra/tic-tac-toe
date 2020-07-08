ac27104678ef53a626e3052e58d5fe8d18197a7f
Marc Wright <marc@generalassemb.ly>
2020-07-08 13:42:41 -0700

    Remove all events for a win condition

--- in-class-solution/script.js
+++ in-class-solution/script.js
@@ -1,23 +1,24 @@
 let playerTurn = "X";
 let moves = ["", "", "", "", "", "", "", "", ""];
+let counter = 0;
 
 // GRAB BOXES
 const boxes = document.querySelectorAll(".box");
 const resetButton = document.querySelector("#reset");
+const messageDiv = document.querySelector(".message-div");
 
 resetButton.addEventListener("click", resetGame);
 
 function resetGame() {
   moves = ["", "", "", "", "", "", "", "", ""];
   playerTurn = "X";
+  counter = 0;
 
   boxes.forEach((box) => {
     box.innerHTML = "";
     box.removeEventListener("click", boxEventListener);
     box.addEventListener("click", boxEventListener, { once: true });
   });
-
-  console.log("reset");
 }
 
 // ADD EVENT LISTENER, FOR EACH LOOP
@@ -25,9 +26,16 @@ boxes.forEach((box) => {
   box.addEventListener("click", boxEventListener, { once: true });
 });
 
+function removeEvents() {
+  boxes.forEach((box) => {
+    box.removeEventListener("click", boxEventListener);
+  });
+}
+
 // CREATE EVENT LISTENER
 function boxEventListener() {
   moves[this.id] = playerTurn;
+  counter++;
   winLogic();
   console.log(moves);
 
@@ -42,50 +50,61 @@ function boxEventListener() {
 
 function winLogic() {
   if (moves[0] === moves[1] && moves[1] === moves[2] && moves[0] !== "") {
-    console.log(`${playerTurn} is the winner on row 1!`);
+    messageDiv.innerHTML = `${playerTurn} is the winner on row 1!`;
+    removeEvents();
   } else if (
     moves[3] === moves[4] &&
     moves[4] === moves[5] &&
     moves[3] !== ""
   ) {
-    console.log(`${playerTurn} is the winner on row 2!`);
+    messageDiv.innerHTML = `${playerTurn} is the winner on row 2!`;
+    removeEvents();
   } else if (
     moves[6] === moves[7] &&
     moves[7] === moves[8] &&
     moves[6] !== ""
   ) {
-    console.log(`${playerTurn} is the winner on row 3!`);
+    messageDiv.innerHTML = `${playerTurn} is the winner on row 3!`;
+    removeEvents();
   } else if (
     moves[0] === moves[3] &&
     moves[3] === moves[6] &&
     moves[0] !== ""
   ) {
-    console.log(`${playerTurn} is the winner in column 1!`);
+    messageDiv.innerHTML = `${playerTurn} is the winner in column 1!`;
+    removeEvents();
   } else if (
     moves[1] === moves[4] &&
     moves[4] === moves[7] &&
     moves[1] !== ""
   ) {
-    console.log(`${playerTurn} is the winner in column 2!`);
+    messageDiv.innerHTML = `${playerTurn} is the winner in column 2!`;
+    removeEvents();
   } else if (
     moves[2] === moves[5] &&
     moves[5] === moves[8] &&
     moves[2] !== ""
   ) {
-    console.log(`${playerTurn} is the winner in column 3!`);
+    messageDiv.innerHTML = `${playerTurn} is the winner in column 3!`;
+    removeEvents();
   } else if (
     moves[0] === moves[4] &&
     moves[4] === moves[8] &&
     moves[0] !== ""
   ) {
-    console.log(`${playerTurn} is the winner on a diagonal`);
+    messageDiv.innerHTML = `${playerTurn} is the winner on a diagonal`;
+    removeEvents();
   } else if (
     moves[2] === moves[4] &&
     moves[4] === moves[6] &&
     moves[2] !== ""
   ) {
-    console.log(`${playerTurn} is the winner on a diagonal`);
+    messageDiv.innerHTML = `${playerTurn} is the winner on a diagonal`;
+    removeEvents();
+  } else if (counter === 9) {
+    messageDiv.innerHTML = "No winner";
+    removeEvents();
   } else {
-    console.log("No winner");
+    messageDiv.innerHTML = "No winner yet";
   }
 }
