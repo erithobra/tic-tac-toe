c02df728ff4801cf66394e05171835b6b1fbff66
ericb2020 <ebrauer83@gmail.com>
2021-01-07 14:30:24 -0600

    Phase 2: auto calculate winner

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ for (let i=0; i<gridSize; i++) {
 // determines if square color should change or remain the same
 for (let i=0; i<gridSize; i++) {
     const oneSquare = document.getElementById(`${i+1}`);
-    console.log(oneSquare);
+    //console.log(oneSquare);
     oneSquare.addEventListener("click", function(e) {
         if (oneSquare.style.background == "red" || oneSquare.style.background == "blue"){
             oneSquare.style.background = oneSquare.style.background;
@@ -27,6 +27,56 @@ for (let i=0; i<gridSize; i++) {
         } else {
         oneSquare.style.background = squareColor;
         }
+        
+       const s1 = (document.getElementById("1").style.background);
+       const s2 = (document.getElementById("2").style.background);
+       const s3 = (document.getElementById("3").style.background);
+       const s4 = (document.getElementById("4").style.background);
+       const s5 = (document.getElementById("5").style.background);
+       const s6 = (document.getElementById("6").style.background);
+       const s7 = (document.getElementById("7").style.background);
+       const s8 = (document.getElementById("8").style.background);
+       const s9 = (document.getElementById("9").style.background);
+       
+
+       if (s1 == s2 && s1 == s3 && s1 !== "") {
+           console.log("they match");
+        } else if (s4 == s5 && s4 == s6 && s4 !== "") {
+        console.log("they match2");
+        } else if (s7 == s8 && s7 == s9 && s7 !== "") {
+        console.log("they match3");
+        } else if (s1 == s4 && s1 == s7 && s1 !== "") {
+            console.log("they match4");
+        } else if (s2 == s5 && s2 == s8 && s2 !== "") {
+            console.log("they match5");
+        } else if (s3 == s6 && s3 == s9 && s3 !== "") {
+            console.log("they match6");
+        } else if (s1 == s5 && s1 == s9 && s1 !== "") {
+            console.log("they match7");
+        } else if (s3 == s5 && s3 == s7 && s3 !== "") {
+            console.log("they match8");
+        }        
+
+
+        // for (let j=0; j<gridSize; j++) {
+        //     const s1 = document.getElementById("1").style.background;
+        //     const s2 = document.getElementById("2").style.background;
+        //     const s3 = document.getElementById("3").style.background;
+        //     const s4 = document.getElementById("4").style.background;
+        //     const s5 = document.getElementById("5").style.background;
+        //     const s6 = document.getElementById("6").style.background;
+        //     const s7 = document.getElementById("7").style.background;
+        //     const s8 = document.getElementById("8").style.background;
+        //     const s9 = document.getElementById("9").style.background;
+            
+        //     if (s1 == s2 && s1 == s3 && s1.value !== "") {
+        //         console.log("winner!!");
+        //         console.log(s1.value);
+        //     } else if (s4 == s5 && s4 == s6 && s4.value !== "") {
+        //         console.log("winner222222");
+        //     }
+        //     console.log(document.getElementById(`${j+1}`).style.background);
+        // }
     })
 }
 
@@ -50,9 +100,34 @@ resetButton.addEventListener("click", function(e) {
     turnCount = 0;
     squareColor = "red";
     for (let i=0; i<gridSize; i++) {
-        document.getElementById(`${i+1}`).style.background = "gray";
+        document.getElementById(`${i+1}`).style.background = "";
     }
 })
 
 
+// win conditions
 
+
+// winArray = [
+//     if (document.getElementById("1").style.background == document.getElementById("2").style.background && document.getElementById("1").style.background !== "gray"){}
+// ]
+
+// console.log(winArray[0]);
+
+/*
+
+1  2  3
+4  5  6
+7  8  9
+
+winning combinations:
+123
+456
+789
+147
+258
+369
+159
+357
+
+*/
